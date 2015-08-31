fa9ad088643b5383b96c0937b1a6567fe8810cad
y_okada <yasokada@gmail.com>
2015-08-31 20:11:33 +0900

    add OnChange() for IF_ipadr1

---
+++ Assets/InputFieldControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputFieldControl : MonoBehaviour {
+
+	public InputField IF_ipadr1;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void OnChangeIpadr1() {
+		SettingKeeperControl.setIpadr1 (IF_ipadr1.text);
+	}
+}
