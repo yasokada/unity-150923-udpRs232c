a52911a6f8f7974e7b90f39fba11545a2a5b0231
y_okada <yasokada@gmail.com>
2015-08-31 20:23:26 +0900

    same for IF_ipadr2

--- Assets/InputFieldControl.cs
+++ Assets/InputFieldControl.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class InputFieldControl : MonoBehaviour {
 
 	public InputField IF_ipadr1;
+	public InputField IF_ipadr2;
 
 	void Start () {
 	
@@ -16,5 +17,8 @@ public class InputFieldControl : MonoBehaviour {
 	
 	public void OnEndEditIpadr1() {
 		SettingKeeperControl.setIpadr1 (IF_ipadr1.text);
+	}
+	public void OnEndEditIpadr2() {
+		SettingKeeperControl.setIpadr2 (IF_ipadr2.text);
 	}
 }
