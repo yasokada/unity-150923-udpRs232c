2db11e6fbd8ef882eaa7e9b2608995c0f1704456
y_okada <yasokada@gmail.com>
2015-09-01 07:11:08 +0900

    WIP

---
+++ Assets/udpMonitorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class udpMonitorScript : MonoBehaviour {
+
+	public Toggle ToggleComm;
+	private bool preToggle = false;
+
+	void Start () {
+	
+	}
+	
+	void Update () {	
+		if (preToggle != ToggleComm.isOn && ToggleComm.isOn) {
+			preToggle = ToggleComm.isOn;
+
+		}
+	}
+}
